ba16bbc4a8310dadb418854a9305fdb753466c14
eyko139 <mud2@gmx.de>
2021-05-23 00:25:09 +0200

    merged heads

---
+++ migrations/versions/4e5791862666_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 4e5791862666
+Revises: e84c74bdf302, 816afd646529
+Create Date: 2021-05-23 00:23:32.109463
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4e5791862666'
+down_revision = ('e84c74bdf302', '816afd646529')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
