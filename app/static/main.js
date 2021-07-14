059b2fbde8bfa8579c4f3cd2f0d0459c8bd5f93f
eyk139 <mud2@gmx.de>
2021-07-14 16:31:04 +0200

    media query

--- app/static/main.js
+++ app/static/main.js
@@ -5,16 +5,18 @@ const todoArrow = document.getElementById("todo-arrow");
 const todox = document.getElementById("todoClick");
 const old = todox.innerHTML;
 const todoHoverHandler = () => {
-    const todo = document.getElementById("row2");
-    const oldInner = todo.innerHTML
-    const todoContainer = document.getElementById("todoContainer");
-    todoContainer.style.gridArea = "1/2/2/4";
-    todo.innerHTML = "You're hovering";
+    if (window.matchMedia("(max-width: 600px)")){
+        const todo = document.getElementById("todoClick");
+        const oldInner = todo.innerHTML
+        const todoContainer = document.getElementById("todoContainer");
+        todoContainer.style.gridArea = "1/2/2/4";
+        todo.innerHTML = "You're hovering";
+    };
 }
 
 const todoLeaverHandler = () => {
     todox.innerHTML = old;
 }
 
-todox.addEventListener("mouseover", todoHoverHandler);
+todox.addEventListener("click", todoHoverHandler);
 todox.addEventListener("mouseleave", todoLeaverHandler);
